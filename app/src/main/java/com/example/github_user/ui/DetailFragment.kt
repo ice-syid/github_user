0a3330a920bd74f0e85389a3f4e4b092b3baed73
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-03-12 21:00:12 +0700

    update ui DetailFragment

--- app/src/main/java/com/example/github_user/ui/DetailFragment.kt
+++ app/src/main/java/com/example/github_user/ui/DetailFragment.kt
@@ -1,12 +1,13 @@
 package com.example.github_user.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.github_user.R
 import com.example.github_user.databinding.FragmentDetailBinding
 import com.example.github_user.model.User
 
@@ -21,6 +22,8 @@ class DetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
         user = args.user
         (activity as AppCompatActivity?)!!.supportActionBar!!.title = user.username
     }
@@ -42,5 +45,40 @@ class DetailFragment : Fragment() {
         binding.tvFollowerValue.setText(user.follower.toString())
         binding.tvFollowingValue.setText(user.following.toString())
         binding.tvRepositoryValue.setText(user.repository.toString())
+
+        binding.tvRepoImg.setImageResource(user.avatar)
+        binding.tvRepoUsername.setText(user.username)
+
+        binding.tvRepositoriesValue.setText(user.repository.toString())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val baseUrl = "https://github.com/"
+        val url = baseUrl + user.username
+
+        return when (item.itemId) {
+            R.id.action_share -> {
+                val action = Intent().apply {
+                    this.action = Intent.ACTION_SEND
+                    this.putExtra(Intent.EXTRA_TEXT, "${user}")
+                    this.type = "text/plain"
+                }
+                startActivity(action)
+                return true
+            }
+            R.id.action_open_browser -> {
+                val action = Intent().apply {
+                    this.action = Intent.ACTION_VIEW
+                    this.setData(Uri.parse(url))
+                }
+                startActivity(action)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
